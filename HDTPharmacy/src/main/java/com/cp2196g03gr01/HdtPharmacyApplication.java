dba53f224d3e73fcf9a834448561e7d606fbf95a
Hiep <hiep2001.job@gmail.com>
2022-09-25 20:36:50 +0700

    First commit

---
+++ HDTPharmacy/src/main/java/com/cp2196g03gr01/HdtPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.cp2196g03gr01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdtPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdtPharmacyApplication.class, args);
+	}
+
+}
